481d2956e04d04f77cb5a3fb60026930144818e1
Jefeated <jeffng@live.ca>
2017-04-17 13:55:52 -0700

    moved git file

---
+++ src/Message.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Message extends React.Component {
+  render() {
+  return (
+
+<main class="messages">
+  <div class="message">
+    <span class="message-username">Anonymous1</span>
+    <span class="message-content">I won't be impressed with technology until I can download food.</span>
+  </div>
+</main>
+
+  )}
+}
+
+export default Message;
--- src/index.jsx
+++ src/index.jsx
@@ -9,3 +9,11 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
+
+var ChattyApp = React.createClass ({
+  render: function(){
+    return
+  }
+
+});
+
